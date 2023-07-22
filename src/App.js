0f5cbc901d3a8417b14374eda5b95bb0bc1802e3
Adarsh <adarshraj10994@gmail.com>
2023-07-22 17:38:33 +0530

    -committed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 
-import './App.css';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+// import About from "./components/About";
+import React , {useState} from 'react' ;
 function App() {
+  const[Mode,setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (Mode === 'light') {
+        setMode('dark');
+        document.body.style.backgroundColor = '#042743';
+    }
+    else {
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+    }
+};
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="container"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur quaerat odit veniam, non, voluptate reiciendis, molestiae illo accusamus totam doloribus neque distinctio! 
-    Enim dignissimos ab voluptas ipsa consectetur. Quae, illo.</p></div>
+    
+    <Navbar title="TextUtils" Mode={Mode} toggleMode = {toggleMode} />
+    <div className="container" my-3="true" > 
+
+    <TextForm heading="Enter the text to analyze below" mode={Mode}/>
+    
+    {/* <About/> */}
+    </div>
+   
     </>
-  );
+       );
 }
 
 export default App;
